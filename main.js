4294cb04d419f263b76f1d7d29f846d8d688bd96
szm123 <szmgitlab123456>
2023-12-19 15:38:40 +0800

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const BirthdayUtil = require('./familyBirthdayCheck')
 const isBirthday = BirthdayUtil.checkDate();
 if (isBirthday==='false'){
   console.log('不是');
+  EmailUtil.sendEmail('2649144933@qq.com','家人生日提醒',"test")
   return;
 }
 const emailText = `今天是${isBirthday}的生日,请及时送上祝福！`
