55043dc3100cd1e6b7f918f9ddff6339e6f9b25e
shizm123 <57487760+shizm123@users.noreply.github.com>
2023-12-19 16:04:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ console.log('username',process.env.USER_NAME)
 console.log('password',process.env.PASS_WORD)
 if (isBirthday==='false'){
   console.log('不是');
-  EmailUtil.sendEmail('2649144933@qq.com','家人生日提醒',"test")
+  // EmailUtil.sendEmail('2649144933@qq.com','家人生日提醒',"test")
   return;
 }
 const emailText = `今天是${isBirthday}的生日,请及时送上祝福！`
